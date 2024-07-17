0428db3da489bbb43deaf1c1766c56a6ad75a426
waka.apps <waka.apps@gmail.com>
2024-07-17 18:03:06 +0900

    buildセットアップ

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./index.ts", // エントリーポイントのファイル
+  output: {
+    path: path.resolve(__dirname, "dist"), // ビルドファイルの出力先
+    filename: "use-vimeo-video-meta.js", // 出力するファイル名
+    libraryTarget: "umd", // UMD形式で出力
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/, // .js, .ts, .tsxファイルに対して
+        exclude: /node_modules/, // node_modulesを除外
+        use: {
+          loader: "babel-loader", // Babelを使う
+        },
+      },
+    ],
+  },
+  externals: {
+    react: "react", // Reactを外部依存として扱う
+  },
+};
